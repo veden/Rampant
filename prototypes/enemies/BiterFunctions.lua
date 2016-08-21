80b926b4a42ad3f138df9f015c201793ba0e7464
veden <veden@ivcore.com>
2016-08-21 14:56:07 -0700

    changed fire spitter sprite to correct one and updated locale

--- prototypes/enemies/BiterFunctions.lua
+++ prototypes/enemies/BiterFunctions.lua
@@ -30,6 +30,37 @@ function biterFunctions.makeBiter(biterAttributes, biterAttack, biterResistances
           }
 end
 
+
+function biterFunctions.makeSpitter(biterAttributes, biterAttack, biterResistances)
+    return {
+                type = "unit",
+                name = biterAttributes.name,
+                icon = "__base__/graphics/icons/small-spitter.png",
+                flags = {"placeable-player", "placeable-enemy", "placeable-off-grid", "breaths-air"},
+                max_health = biterAttributes.health,
+                order = "b-b-a",
+                subgroup="enemies",
+                healing_per_tick = biterAttributes.healing,
+                resistances = biterResistances,
+                collision_box = {{-0.4 * biterAttributes.scale, -0.4 * biterAttributes.scale}, 
+                                 {0.4 * biterAttributes.scale, 0.4 * biterAttributes.scale}},
+                selection_box = {{-0.7 * biterAttributes.scale, -1.5 * biterAttributes.scale}, 
+                                 {0.7 * biterAttributes.scale, 0.3 * biterAttributes.scale}},
+                sticker_box = {{-0.6 * biterAttributes.scale, -0.8 * biterAttributes.scale}, 
+                               {0.6 * biterAttributes.scale, 0}},
+                attack_parameters = biterAttack,
+                vision_distance = 30,
+                movement_speed = biterAttributes.movement,
+                distance_per_frame = 0.1,
+                pollution_to_join_attack = 200,
+                distraction_cooldown = 300,
+                dying_explosion = biterAttributes.explosion,
+                dying_sound =  make_biter_dying_sounds(1.0),
+                working_sound =  make_biter_calls(0.7),
+                run_animation = spitterrunanimation(biterAttributes.scale, biterAttributes.tint1, biterAttributes.tint2)
+          }
+end
+
 function biterFunctions.createSuicideAttack(attributes)
     return { type = "projectile",
              range = 0.5,
@@ -86,9 +117,9 @@ function biterFunctions.createFireAttack(attributes)
               turn_range = attributes.turnRange,
               fire_penalty = attributes.firePenalty,
               
-              animation = biterattackanimation(attributes.scale, 
-                                               attributes.tint1, 
-                                               attributes.tint2),
+              animation = spitterattackanimation(attributes.scale, 
+                                                attributes.tint1, 
+                                                attributes.tint2),
               gun_barrel_length = 2 * attributes.scale,
               gun_center_shift = {
                   north = {0, -0.65 * attributes.scale},
