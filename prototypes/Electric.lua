3d4bfcb2fadaf2f28a010e0897cfa7ebe92d835a
Aaron Veden <veden@vedbyte.com>
2018-02-03 22:54:11 -0800

    updated electric faction

--- prototypes/Electric.lua
+++ prototypes/Electric.lua
@@ -2,6 +2,7 @@
 
 local biterUtils = require("utils/BiterUtils")
 local beamUtils = require("utils/BeamUtils")
+local attackBall = require("utils/AttackBall")
 local swarmUtils = require("SwarmUtils")
 package.path = "../libs/?.lua;" .. package.path
 local constants = require("Constants")
@@ -22,7 +23,11 @@ local ELECTRIC_WORM_VARIATIONS = constants.ELECTRIC_WORM_VARIATIONS
 local buildUnitSpawner = swarmUtils.buildUnitSpawner
 local buildWorm = swarmUtils.buildWorm
 local createElectricAttack = biterUtils.createElectricAttack
+local createAttackBall = attackBall.createAttackBall
+local makeLaser = beamUtils.makeLaser
+local createStreamAttack = biterUtils.createStreamAttack
 local makeBeam = beamUtils.makeBeam
+local makeBubble = beamUtils.makeBubble
 
 local makeUnitAlienLootTable = biterUtils.makeUnitAlienLootTable
 local makeSpawnerAlienLootTable = biterUtils.makeSpawnerAlienLootTable
@@ -32,6 +37,10 @@ local biterLoot = makeUnitAlienLootTable("blue")
 local spawnerLoot = makeSpawnerAlienLootTable("blue")
 local wormLoot = makeWormAlienLootTable("blue")
 
+local electricBubble = makeBubble({
+	name = "electric-worm",
+	lTint = {r=0, g=0.1, b=1, a=1}
+})
 
 
 -- electric biters
@@ -346,16 +355,16 @@ buildUnitSpawner(
 	    {
 		type = "attack",
 		name = "range",
-		[1] = 7,
-		[2] = 7,
-		[3] = 8,
-		[4] = 8,
-		[5] = 9,
-		[6] = 9,
-		[7] = 10,
-		[8] = 10,
-		[9] = 11,
-		[10] = 11
+		[1] = 11,
+		[2] = 11,
+		[3] = 12,
+		[4] = 12,
+		[5] = 13,
+		[6] = 13,
+		[7] = 14,
+		[8] = 14,
+		[9] = 15,
+		[10] = 15
 	    }
 	},
 	
@@ -622,7 +631,33 @@ buildWorm(
 	loot = wormLoot,
 	attributes = {},
 	attack = {
-	    damageType = "electric"
+	    bubble = electricBubble,
+	    damageType = "electric",
+	    pointEffects = function(attributes)
+		return 
+		    {
+			{
+			    type="nested-result",
+			    action = {
+				{
+				    type = "cluster",
+				    cluster_count = attributes.clusters,
+				    distance = attributes.clusterDistance,
+				    distance_deviation = 3,
+				    action_delivery =
+					{
+					    type = "projectile",
+					    projectile = attributes.laserName,
+					    duration = 20,
+					    direction_deviation = 0.6,
+					    starting_speed = attributes.startingSpeed,
+					    starting_speed_deviation = 0.3
+					}
+				}
+			    },
+			}
+		    }
+	    end
 	},
 	resistances = {},
 
@@ -639,10 +674,58 @@ buildWorm(
 	    [10] = 1.4
 	},
 	attackName = "worm-electric",
-	tint = {r=0, g=0.25, b=0.83, a=0.65}
+	tint = {r=0, g=0.25, b=0.83, a=0.65},
+	pTint = {r=0, g=0.1, b=1, a=1},
+	sTint = {r=0, g=0.1, b=1, a=1},
+	lTint = {r=0, g=0.1, b=1, a=1}
     },
 
     {
+	{
+	    type = "attack",
+	    name = "startingSpeed",
+	    [1] = 0.25,
+	    [2] = 0.25,
+	    [3] = 0.27,
+	    [4] = 0.27,
+	    [5] = 0.29,
+	    [6] = 0.29,
+	    [7] = 0.31,
+	    [8] = 0.31,
+	    [9] = 0.33,
+	    [10] = 0.33
+	},
+	
+	{
+	    type = "attack",
+	    name = "clusterDistance",
+	    [1] = 3,
+	    [2] = 3,
+	    [3] = 4,
+	    [4] = 4,
+	    [5] = 5,
+	    [6] = 5,
+	    [7] = 6,
+	    [8] = 6,
+	    [9] = 7,
+	    [10] = 7
+	},
+	
+	{
+	    type = "attack",
+	    name = "clusters",
+	    [1] = 5,
+	    [2] = 5,
+	    [3] = 6,
+	    [4] = 6,
+	    [5] = 7,
+	    [6] = 7,
+	    [7] = 8,
+	    [8] = 8,
+	    [9] = 9,
+	    [10] = 9
+	},
+	
 	{
 	    type = "attribute",
 	    name = "health",
@@ -882,54 +965,26 @@ buildWorm(
 	    [10] = 32
 	},
 
-	{	    
-	    type = "attack",
-	    name = "width",
-	    [1] = 0.5,
-	    [2] = 0.5,
-	    [3] = 0.6,
-	    [4] = 0.6,
-	    [5] = 0.7,
-	    [6] = 0.7,
-	    [7] = 0.8,
-	    [8] = 0.8,
-	    [9] = 0.9,
-	    [10] = 0.9	    
-	},
-
-	{   
-	    type = "attack",
-	    name = "damageInterval",
-	    [1] = 20,
-	    [2] = 20,
-	    [3] = 21,
-	    [4] = 21,
-	    [5] = 22,
-	    [6] = 22,
-	    [7] = 23,
-	    [8] = 23,
-	    [9] = 24,
-	    [10] = 24	    
-	},
-
 	{
 	    type = "attack",
-	    name = "duration",
-	    [1] = 20,
-	    [2] = 20,
-	    [3] = 21,
-	    [4] = 21,
-	    [5] = 22,
-	    [6] = 22,
-	    [7] = 23,
-	    [8] = 23,
-	    [9] = 24,
-	    [10] = 24	    
+	    name = "radius",
+	    [1] = 1.5,
+	    [2] = 1.6,
+	    [3] = 1.7,
+	    [4] = 1.8,
+	    [5] = 1.9,
+	    [6] = 2.0,
+	    [7] = 2.2,
+	    [8] = 2.3,
+	    [9] = 2.5,
+	    [10] = 3.0
 	}
     },
     
     function (attributes)
-	return createElectricAttack(attributes, makeBeam(attributes))
+	attributes.laserName = makeLaser(attributes)
+	return createStreamAttack(attributes,
+				  createAttackBall(attributes))
     end,
 
     ELECTRIC_WORM_VARIATIONS,
--- prototypes/utils/BeamUtils.lua
+++ prototypes/utils/BeamUtils.lua
@@ -1,150 +1,219 @@
 local beamUtils = {}
 
+function beamUtils.makeBubble(attributes)
+    local name = attributes.name .. "-bubble-rampant"
+    data:extend({{
+		type = "explosion",
+		name = name,
+		flags = {"not-on-map"},
+		animation_speed = 1,
+		animations =
+		    {
+			{
+			    filename = "__base__/graphics/entity/laser-bubble/laser-bubble.png",
+			    priority = "extra-high",
+			    width = 8,
+			    height = 8,
+			    frame_count = 5
+			}
+		    },
+		light = {intensity = 1, size = 10, color = attributes.lTint or {r = 1.0, g = 1.0, b = 1.0}},
+		smoke = "smoke-fast",
+		smoke_count = 2,
+		smoke_slow_down_factor = 1
+    }})
+    return name
+end
+
+function beamUtils.makeLaser(attributes)
+    local name = attributes.name .. "-laser-rampant"
+    data:extend({{
+		type = "projectile",
+		name = name ,
+		flags = {"not-on-map"},
+		collision_box = attributes.collisionBox or {{-0.3, -1.1}, {0.3, 1.1}},
+		acceleration = attributes.acceleration or 0.03,
+		action =
+		    {
+			type = "direct",
+			action_delivery =
+			    {
+				type = "instant",
+				target_effects =
+				    {
+					{
+					    type = "create-entity",
+					    entity_name = attributes.bubble or "laser-bubble"
+					},
+					{
+					    type = "damage",
+					    damage = { amount = attributes.damage or 5, type = attributes.damageType or "laser"}
+					}
+				    }
+			    }
+		    },
+		light = {intensity = 0.5, size = 10},
+		animation =
+		    {
+			filename = "__base__/graphics/entity/laser/laser-to-tint-medium.png",
+			tint = attributes.lTint or {r=1.0, g=0.0, b=0.0},
+			frame_count = 1,
+			width = 12,
+			height = 33,
+			priority = "high",
+			blend_mode = "additive"
+		    },
+		speed = 0.15
+    }})
+    return name
+end
+
 function beamUtils.makeBeam(attributes)
-  local result =
-  {
-    type = "beam",
-    flags = {"not-on-map"},
-    width = attributes.width or 0.5,
-    damage_interval = attributes.damageInterval or 20,
-    action =
-    {
-      type = "direct",
-      action_delivery =
-      {
-        type = "instant",
-        target_effects =
-        {
-          {
-            type = "damage",
-            damage = { amount = attributes.damage or 10, type = attributes.damageType or "electric"}
-          }
-        }
-      }
-    },
-    start = 
-    { 
-      filename = "__base__/graphics/entity/beam/tileable-beam-START.png",
-      line_length = 4,
-      width = 52,
-      height = 40,
-      frame_count = 16,
-      axially_symmetrical = false,
-      direction_count = 1,
-      shift = {-0.03125, 0},
-      hr_version = {
-        filename = "__base__/graphics/entity/beam/hr-tileable-beam-START.png",
-        line_length = 4,
-        width = 94,
-        height = 66,
-        frame_count = 16,
-        axially_symmetrical = false,
-        direction_count = 1,
-        shift = {0.53125, 0},
-        scale = 0.5,
-      }
-    },
-    ending = 
-    { 
-      filename = "__base__/graphics/entity/beam/tileable-beam-END.png",
-      line_length = 4,
-      width = 49,
-      height = 54,
-      frame_count = 16,
-      axially_symmetrical = false,
-      direction_count = 1,
-      shift = {-0.046875, 0},
-      hr_version = { 
-        filename = "__base__/graphics/entity/beam/hr-tileable-beam-END.png",
-        line_length = 4,
-        width = 91,
-        height = 93,
-        frame_count = 16,
-        axially_symmetrical = false,
-        direction_count = 1,
-        shift = {-0.078125, -0.046875},
-        scale = 0.5,
-      } 
-    },
-    head =
-    {
-      filename = "__base__/graphics/entity/beam/beam-head.png",
-      line_length = 16,
-      width = 45,
-      height = 39,
-      frame_count = 16,
-      animation_speed = 0.5,
-      blend_mode = "additive-soft",
-    },
-    tail =
-    {
-      filename = "__base__/graphics/entity/beam/beam-tail.png",
-      line_length = 16,
-      width = 45,
-      height = 39,
-      frame_count = 16,
-      blend_mode = "additive-soft",
-    },
-    body =
-    {
-      {
-        filename = "__base__/graphics/entity/beam/beam-body-1.png",
-        line_length = 16,
-        width = 45,
-        height = 39,
-        frame_count = 16,
-        blend_mode = "additive-soft",
-      },
-      {
-        filename = "__base__/graphics/entity/beam/beam-body-2.png",
-        line_length = 16,
-        width = 45,
-        height = 39,
-        frame_count = 16,
-        blend_mode = "additive-soft",
-      },
-      {
-        filename = "__base__/graphics/entity/beam/beam-body-3.png",
-        line_length = 16,
-        width = 45,
-        height = 39,
-        frame_count = 16,
-        blend_mode = "additive-soft",
-      },
-      {
-        filename = "__base__/graphics/entity/beam/beam-body-4.png",
-        line_length = 16,
-        width = 45,
-        height = 39,
-        frame_count = 16,
-        blend_mode = "additive-soft",
-      },
-      {
-        filename = "__base__/graphics/entity/beam/beam-body-5.png",
-        line_length = 16,
-        width = 45,
-        height = 39,
-        frame_count = 16,
-        blend_mode = "additive-soft",
-      },
-      {
-        filename = "__base__/graphics/entity/beam/beam-body-6.png",
-        line_length = 16,
-        width = 45,
-        height = 39,
-        frame_count = 16,
-        blend_mode = "additive-soft",
-      },
-    }
-  }
+    local result =
+	{
+	    type = "beam",
+	    flags = {"not-on-map"},
+	    width = attributes.width or 0.5,
+	    collision_box = attributes.collisionBox or {{-0.3, -1.1}, {0.3, 1.1}},
+	    damage_interval = attributes.damageInterval or 20,
+	    action =
+		{
+		    type = "direct",
+		    action_delivery =
+			{
+			    type = "instant",
+			    target_effects =
+				{
+				    {
+					type = "damage",
+					damage = { amount = attributes.damage or 10, type = attributes.damageType or "electric"}
+				    }
+				}
+			}
+		},
+	    start = 
+		{ 
+		    filename = "__base__/graphics/entity/beam/tileable-beam-START.png",
+		    line_length = 4,
+		    width = 52,
+		    height = 40,
+		    frame_count = 16,
+		    axially_symmetrical = false,
+		    direction_count = 1,
+		    shift = {-0.03125, 0},
+		    hr_version = {
+			filename = "__base__/graphics/entity/beam/hr-tileable-beam-START.png",
+			line_length = 4,
+			width = 94,
+			height = 66,
+			frame_count = 16,
+			axially_symmetrical = false,
+			direction_count = 1,
+			shift = {0.53125, 0},
+			scale = 0.5,
+		    }
+		},
+	    ending = 
+		{ 
+		    filename = "__base__/graphics/entity/beam/tileable-beam-END.png",
+		    line_length = 4,
+		    width = 49,
+		    height = 54,
+		    frame_count = 16,
+		    axially_symmetrical = false,
+		    direction_count = 1,
+		    shift = {-0.046875, 0},
+		    hr_version = { 
+			filename = "__base__/graphics/entity/beam/hr-tileable-beam-END.png",
+			line_length = 4,
+			width = 91,
+			height = 93,
+			frame_count = 16,
+			axially_symmetrical = false,
+			direction_count = 1,
+			shift = {-0.078125, -0.046875},
+			scale = 0.5,
+		    } 
+		},
+	    head =
+		{
+		    filename = "__base__/graphics/entity/beam/beam-head.png",
+		    line_length = 16,
+		    width = 45,
+		    height = 39,
+		    frame_count = 16,
+		    animation_speed = 0.5,
+		    blend_mode = "additive-soft",
+		},
+	    tail =
+		{
+		    filename = "__base__/graphics/entity/beam/beam-tail.png",
+		    line_length = 16,
+		    width = 45,
+		    height = 39,
+		    frame_count = 16,
+		    blend_mode = "additive-soft",
+		},
+	    body =
+		{
+		    {
+			filename = "__base__/graphics/entity/beam/beam-body-1.png",
+			line_length = 16,
+			width = 45,
+			height = 39,
+			frame_count = 16,
+			blend_mode = "additive-soft",
+		    },
+		    {
+			filename = "__base__/graphics/entity/beam/beam-body-2.png",
+			line_length = 16,
+			width = 45,
+			height = 39,
+			frame_count = 16,
+			blend_mode = "additive-soft",
+		    },
+		    {
+			filename = "__base__/graphics/entity/beam/beam-body-3.png",
+			line_length = 16,
+			width = 45,
+			height = 39,
+			frame_count = 16,
+			blend_mode = "additive-soft",
+		    },
+		    {
+			filename = "__base__/graphics/entity/beam/beam-body-4.png",
+			line_length = 16,
+			width = 45,
+			height = 39,
+			frame_count = 16,
+			blend_mode = "additive-soft",
+		    },
+		    {
+			filename = "__base__/graphics/entity/beam/beam-body-5.png",
+			line_length = 16,
+			width = 45,
+			height = 39,
+			frame_count = 16,
+			blend_mode = "additive-soft",
+		    },
+		    {
+			filename = "__base__/graphics/entity/beam/beam-body-6.png",
+			line_length = 16,
+			width = 45,
+			height = 39,
+			frame_count = 16,
+			blend_mode = "additive-soft",
+		    },
+		}
+	}
 
     result.working_sound =
-    {
-      {
-        filename = "__base__/sound/fight/electric-beam.ogg",
-        volume = 0.7
-      }
-    }
+	{
+	    {
+		filename = "__base__/sound/fight/electric-beam.ogg",
+		volume = 0.7
+	    }
+	}
 
     local name = attributes.name .. "-beam-rampant"
     result.name = name
