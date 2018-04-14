2fe032d23a19ebaaefbaf65afaff48048b8ea1ad
Aaron Veden <veden@vedbyte.com>
2018-04-14 16:38:16 -0700

    fix requires and tweak infernos

--- Upgrade.lua
+++ Upgrade.lua
@@ -217,10 +217,10 @@ function upgrade.attempt(natives)
 	game.surfaces[1].print("Rampant - Version 0.16.22")
 	global.version = constants.VERSION_57
     end
-    if (global.version < constants.VERSION_58) then
+    if (global.version < constants.VERSION_59) then
 
-	game.surfaces[1].print("Rampant - Version 0.16.23")
-	global.version = constants.VERSION_58
+	game.surfaces[1].print("Rampant - Version 0.16.24")
+	global.version = constants.VERSION_59
     end
 
     
