942f5ead6bc8f0adf680170c3f8f3d082802f75b
Aaron Veden <veden@vedbyte.com>
2019-10-27 17:40:07 -0700

    removed garbage

--- prototypes/buildings/ChunkScanner.lua
+++ prototypes/buildings/ChunkScanner.lua
@@ -48,7 +48,6 @@ for t=1,11 do
             {
                 type = "simple-entity-with-force",
                 name = "chunk-scanner-" .. t .. "-nest-rampant",
-                -- render_layer = "object",
                 icon = "__base__/graphics/icons/steel-chest.png",
                 icon_size = 32,
                 flags = {},
@@ -67,35 +66,6 @@ for t=1,11 do
                         height = 1
                     }
             }
-            -- {
-            --     type = "container",
-            --     name = "chunk-scanner-" .. t .. "-nest-rampant",
-            --     -- icon = "__base__/graphics/icons/wooden-chest.png",
-            --     -- icon_size = 32,
-            --     flags = {},
-            --     collision_mask = {"player-layer", "object-layer", "water-tile"},
-            --     collision_box = {{-3 * scale, -2 * scale}, {2 * scale, 2 * scale}},
-            --     selection_box = {{-3 * scale, -2 * scale}, {2 * scale, 2 * scale}},
-            --     minable = nil,
-            --     max_health = 100,
-            --     corpse = nil,
-            --     fast_replaceable_group = "container",
-            --     inventory_size = 16,
-            --     open_sound = nil,
-            --     close_sound = nil,
-            --     vehicle_impact_sound =  nil,
-            --     picture =
-            --         {
-            --             filename = "__core__/graphics/empty.png",
-            --             priority = "extra-high",
-            --             width = 1,
-            --             height = 1,
-            --             shift = util.by_pixel(0.5, -2)
-            --         },
-            --     circuit_wire_connection_point = circuit_connector_definitions["chest"].points,
-            --     circuit_connector_sprites = circuit_connector_definitions["chest"].sprites,
-            --     circuit_wire_max_distance = default_circuit_wire_max_distance
-            -- }
         }
     )
 end
@@ -138,38 +108,9 @@ for x=1,#types do
 
             data:extend(
                 {
-                    --     {
-                    --         type = "container",
-                    --         name = "chunk-scanner-" .. t .. "-" .. name .. "-nest-rampant",
-                    --         -- icon = "__base__/graphics/icons/wooden-chest.png",
-                    --         -- icon_size = 32,
-                    --         flags = {},
-                    --         collision_mask = {"player-layer", "object-layer", "water-tile"},
-                    --         collision_box = {{-3 * scale, -2 * scale}, {2 * scale, 2 * scale}},
-                    --         selection_box = {{-3 * scale, -2 * scale}, {2 * scale, 2 * scale}},
-                    --         minable = nil,
-                    --         max_health = 1,
-                    --         corpse = nil,
-                    --         fast_replaceable_group = "container",
-                    --         inventory_size = 1,
-                    --         open_sound = nil,
-                    --         close_sound = nil,
-                    --         vehicle_impact_sound = nil,
-                    --         picture =
-                    --             {
-                    --                 filename = "__core__/graphics/empty.png",
-                    --                 priority = "extra-high",
-                    --                 width = 1,
-                    --                 height = 1
-                    --             },
-                    --         circuit_wire_connection_point = circuit_connector_definitions["chest"].points,
-                    --         circuit_connector_sprites = circuit_connector_definitions["chest"].sprites,
-                    --         circuit_wire_max_distance = default_circuit_wire_max_distance
-                    --     }
                     {
                         type = "simple-entity-with-force",
                         name = "entity-proxy-" .. t .. "-" .. name .. "-" .. st .. "-rampant",
-                        -- render_layer = "object",
                         icon = "__base__/graphics/icons/steel-chest.png",
                         icon_size = 32,
                         flags = {},
