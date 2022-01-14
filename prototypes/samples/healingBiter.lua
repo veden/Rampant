909104a3e4cf54526cbb1cbefee788b2648fb72f
Aaron Veden <veden@vedbyte.com>
2022-01-14 14:08:58 -0800

    FACTO-19: Switching license to GPLv3

--- prototypes/samples/healingBiter.lua
+++ prototypes/samples/healingBiter.lua
@@ -1,3 +1,19 @@
+-- Copyright (C) 2022  veden
+
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 local smallbiterscale = 0.5
 local small_biter_tint1 = {r=0.56, g=0.46, b=0.42, a=0.65}
 local small_biter_tint2 = {r=1, g=0.63, b=0, a=0.4}
